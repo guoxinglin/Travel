c080a730078f3546a9c68248a77680da7743a5e1
guoxinglin <875772109@qq.com>
2018-08-18 23:58:21 +0800

    icons

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,4 @@ export default {
 </script>
 
 <style>
- #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  font-size: 0.3rem ;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
 </style>
