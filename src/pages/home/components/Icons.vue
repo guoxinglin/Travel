bc786414e44548f28c64ab1fb8e2693975b2d911
guoxinglin <875772109@qq.com>
2018-08-22 22:18:30 +0800

    weekend

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -47,7 +47,7 @@ export default {
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
         desc: '景点门票'
       }, {
-        id: '0007',
+        id: '00011',
         imgUrl: 'http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png',
         desc: '景点门票'
       }, {
@@ -77,40 +77,40 @@ export default {
 }
 </script>
 <style lang="stylus" scoped>
-    @import '~styles/varibles.styl';
-    @import '~styles/mixins.styl';
-    .icons >>> .swiper-container
-      height :0
-      padding-bottom :50%
+  @import '~styles/varibles.styl';
+  @import '~styles/mixins.styl';
+  .icons >>> .swiper-container
+    height :0
+    padding-bottom :50%
+  .icons
+    margin-top : .1rem
     .icon
-        position: relative
-        float: left
-        width : 25%
-        padding-bottom :25%
-        overflow: hidden
-        height :0
-        .icon-img
-            position :absolute
-            top: 0
-            left: 0
-            right: 0
-            bottom : .44rem
-            box-sizing :border-box
-            padding: .1rem
-            margin:0  auto
-            .icon-img-content
-                display :block
-                height : 100%
-                margin : 0 auto
-                /*margin:5px 15px */
-    .icon-desc
-        position : absolute
-        left:0
-        right :0
-        bottom:0
-        height :.44rem
-        line-height : .44rem
-        color : $darkTextColor;
-        text-align :center
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+            display: block
+            margin: 0 auto
+            height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
         ellipsis()
 </style>
